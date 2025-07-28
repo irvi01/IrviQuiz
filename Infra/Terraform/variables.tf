9343b335cd3ec323d801ddf7f8a1ec03201c11e3
Yrwing <yrwing.new@gmail.com>
2025-07-28 11:25:34 -0300

    Add k8s manifests for ArgoCD

--- Infra/Terraform/variables.tf
+++ Infra/Terraform/variables.tf
@@ -12,3 +12,24 @@ variable "aks_cluster_name" {
   description = "AKS Cluster name"
   default     = "aks-irviquiz-dev"
 }
+
+variable "client_id" {
+  description = "Client ID for the AKS cluster"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Client Secret for the AKS cluster"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
